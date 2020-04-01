1abb3b86520bf1fef684d71a7a279bb09f6e3781
AlphaniaMuthee <walphania2@gmail.com>
2020-04-01 22:00:56 +0300

    add changes made to file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DisplayComponent } from '../app/display/display.component'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  title = 'quotes';
 }
